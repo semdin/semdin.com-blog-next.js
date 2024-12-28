9c06b7f2638ee0aa7702947c79987d48ffa422dc
edanexmehmet <mehmet@e-danex.com>
2024-12-28 08:32:52 +0300

    Add initial components and utilities for layout and theming

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div>
+      <h1>semdin.com</h1>
+      <ul className="flex justify-between">
+        <li>Home</li>
+        <li>About</li>
+        <li>Technology</li>
+        <li>Algorithm</li>
+        <li>Posts</li>
+      </ul>
+    </div>
+  );
+};
